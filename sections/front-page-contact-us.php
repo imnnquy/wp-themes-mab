8f5380aa5ba33edea2c165b539caa02b82eafd55
Quy Nguyen <qnguyen50@csc.com>
2016-10-22 21:58:47 +0700

    Complete

--- sections/front-page-contact-us.php
+++ sections/front-page-contact-us.php
@@ -51,110 +51,20 @@ if ( $general_title != '' || $general_entry != '' || $general_address_title != '
      || $general_contact_form_7 != null && $general_contact_form_7 != 'default' ) {
 
 	?>
-	<section id="contact-us" class="front-page-section">
-		<?php if ( $general_title || $general_entry ): ?>
-			<div class="section-header">
-				<div class="container">
-					<div class="row">
-						<?php if ( $general_title ): ?>
-							<div class="col-sm-12">
-								<h3><?php echo illdy_sanitize_html( $general_title ); ?></h3>
-							</div><!--/.col-sm-12-->
-						<?php endif; ?>
-						<?php if ( $general_entry ): ?>
-							<div class="col-sm-10 col-sm-offset-1">
-								<p><?php echo illdy_sanitize_html( $general_entry ); ?></p>
-							</div><!--/.col-sm-10.col-sm-offset-1-->
-						<?php endif; ?>
-					</div><!--/.row-->
-				</div><!--/.container-->
-			</div><!--/.section-header-->
-		<?php endif; ?>
-		<div class="section-content">
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-12">
-						<div class="row" style="margin-bottom: 45px;">
-							<div class="col-sm-4">
-								<div class="contact-us-box">
-									<?php if ( $general_address_title ): ?>
-										<div class="box-left" data-customizer="box-left-address-title">
-											<?php echo illdy_sanitize_html( $general_address_title ); ?>
-										</div><!--/.box-left-->
-									<?php endif; ?>
-									<div class="box-right">
-										<?php if ( $address1 ): ?>
-											<span class="box-right-row" data-customizer="contact-us-address-1"><?php echo illdy_sanitize_html( $address1 ); ?></span>
-										<?php endif; ?>
-										<?php if ( $address2 ): ?>
-											<span class="box-right-row" data-customizer="contact-us-address-2"><?php echo illdy_sanitize_html( $address2 ); ?></span>
-										<?php endif; ?>
-									</div><!--/.box-right-->
-								</div><!--/.contact-us-box-->
-							</div><!--/.col-sm-4-->
-							<div class="col-sm-5">
-								<div class="contact-us-box">
-									<?php if ( $customer_support_title ): ?>
-										<div class="box-left" data-customizer="box-left-customer-support-title">
-											<?php echo illdy_sanitize_html( $customer_support_title ); ?>
-										</div><!--/.box-left-->
-									<?php endif; ?>
-									<div class="box-right">
-										<?php if ( $email ): ?>
-											<span class="box-right-row"><?php _e( 'E-mail:', 'illdy' ); ?>
-												<a href="mailto:<?php echo esc_attr( $email ); ?>" title="<?php echo esc_attr( $email ); ?>"><?php echo esc_html( $email ); ?></a></span>
-										<?php endif; ?>
-										<?php if ( $phone ): ?>
-											<span class="box-right-row" data-customizer="contact-us-phone"><?php _e( 'Phone:', 'illdy' ); ?><?php echo illdy_sanitize_html( $phone ); ?></span>
-										<?php endif; ?>
-									</div><!--/.box-right-->
-								</div><!--/.contact-us-box-->
-							</div><!--/.col-sm-5-->
-							<div class="col-sm-3">
-								<?php if ( $contact_bar_twitter_url || $contact_bar_facebook_url || $contact_bar_linkedin_url || $contact_bar_googlep_url || $contact_bar_pinterest_url || $contact_bar_instagram_url || $contact_bar_youtube_url || $contact_bar_vimeo_url ) { ?>
-									<div class="contact-us-social">
-										<?php if ( $contact_bar_twitter_url ): ?>
-											<a href="<?php echo esc_url( $contact_bar_twitter_url ); ?>" title="<?php _e( 'Twitter', 'illdy' ); ?>" target="_blank"><i class="fa fa-twitter"></i></a>
-										<?php endif; ?>
-										<?php if ( $contact_bar_facebook_url ): ?>
-											<a href="<?php echo esc_url( $contact_bar_facebook_url ); ?>" title="<?php _e( 'Facebook', 'illdy' ); ?>" target="_blank"><i class="fa fa-facebook"></i></a>
-										<?php endif; ?>
-										<?php if ( $contact_bar_linkedin_url ): ?>
-											<a href="<?php echo esc_url( $contact_bar_linkedin_url ); ?>" title="<?php _e( 'LinkedIn', 'illdy' ); ?>" target="_blank"><i class="fa fa-linkedin"></i></a>
-										<?php endif; ?>
-										<?php if ( $contact_bar_googlep_url ): ?>
-											<a href="<?php echo esc_url( $contact_bar_googlep_url ); ?>" title="<?php _e( 'Google+', 'illdy' ); ?>" target="_blank"><i class="fa fa-google-plus"></i></a>
-										<?php endif; ?>
-										<?php if ( $contact_bar_pinterest_url ): ?>
-											<a href="<?php echo esc_url( $contact_bar_pinterest_url ); ?>" title="<?php _e( 'Pinterest', 'illdy' ); ?>" target="_blank"><i class="fa fa-pinterest"></i></a>
-										<?php endif; ?>
-										<?php if ( $contact_bar_instagram_url ): ?>
-											<a href="<?php echo esc_url( $contact_bar_instagram_url ); ?>" title="<?php _e( 'Instagram', 'illdy' ); ?>" target="_blank"><i class="fa fa-instagram"></i></a>
-										<?php endif; ?>
-										<?php if ( $contact_bar_youtube_url ): ?>
-											<a href="<?php echo esc_url( $contact_bar_youtube_url ); ?>" title="<?php _e( 'YouTube', 'illdy' ); ?>" target="_blank"><i class="fa fa-youtube"></i></a>
-										<?php endif; ?>
-										<?php if ( $contact_bar_vimeo_url ): ?>
-											<a href="<?php echo esc_url( $contact_bar_vimeo_url ); ?>" title="<?php _e( 'Vimeo', 'illdy' ); ?>" target="_blank"><i class="fa fa-vimeo"></i></a>
-										<?php endif; ?>
-									</div><!--/.contact-us-social-->
-								<?php } ?>
-							</div><!--/.col-sm-3-->
-						</div><!--/.row-->
-					</div><!--/.col-sm-12-->
-				</div><!--/.row-->
-				<div class="row">
-					<div class="col-sm-12">
-						<?php if ( class_exists( 'WPCF7' ) && $general_contact_form_7 != null && $general_contact_form_7 != 'default' ): ?>
-							<?php $shortcode = '[contact-form-7 id="' . esc_html( $general_contact_form_7 ) . '"]'; ?>
-							<?php echo do_shortcode( $shortcode ); ?>
-						<?php endif; ?>
-					</div><!--/.col-sm-12-->
-				</div><!--/.row-->
-			</div><!--/.container-->
-		</div><!--/.section-content-->
-	</section><!--/#contact-us.front-page-section-->
-
+        <section id="newsletter-section" class="front-page-section">
+            <!--/.section-header-->
+            <div class="mab-email-input">
+            	<input type="email" name="email" placeholder="Email">
+            </div>
+            <a href="#" title="Subscribe Newsletter" class="subscribe-button"><i class="fa fa-bell"></i>Subscribe Newsletter</a>
+            <div class="section-content">
+                <div class="container">
+                    
+                </div>
+                <!--/.container-->
+            </div>
+            <!--/.section-content-->
+        </section>
 	<section id="tarif" class="front-page-section" style="background-image: url(/wp-content/uploads/2016/10/OB823N0.jpg);">
 	    <div class="tarif-overlay"></div>
 	    <div class="container">
@@ -172,7 +82,10 @@ if ( $general_title != '' || $general_entry != '' || $general_address_title != '
 	        <!--/.row-->
 	        
 	    </div>
-	    <div class="container">
+	    <!--/.container-->
+	</section>
+	<section id="contact-us-social">
+		<div class="container">
 	    	<div class="row">
 	        	<a href="<?php echo esc_url( $contact_bar_facebook_url ); ?>" title="<?php _e( 'Facebook', 'illdy' ); ?>" target="_blank">
 	        		<img src="<?php echo get_template_directory_uri() ?>/layout/images/front-page/14.png">
@@ -181,10 +94,9 @@ if ( $general_title != '' || $general_entry != '' || $general_address_title != '
 	        		<img src="<?php echo get_template_directory_uri() ?>/layout/images/front-page/16.png">
 	        	</a>
 	        </div>
-	        <div class="row">mesartisansbeaute</div>
+	        <div class="row footer-text">mesartisansbeaute</div>
 	    </div>
-	    <!--/.container-->
-	</section>
+    </section>
 
 
 <?php } ?>
